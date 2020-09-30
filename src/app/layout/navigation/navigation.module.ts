27f7b2d97540d5c1d2dada887a1c946310cec37c
jorgebarrero <jorgebarrero@gmail.com>
2020-09-30 07:36:57 -0400

    Fix: navigation

--- src/app/layout/navigation/navigation.module.ts
+++ src/app/layout/navigation/navigation.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-menuimport { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 // Components and routing
 import { NavigationRoutingModule } from './navigation.routing';
